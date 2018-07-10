e504604999eff679e7863ad113112d07046383dd
Ana Bebic <anabebic.x@gmail.com>
2018-07-10 20:13:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,8 +57,13 @@ def self.new_from_filename(filename)
 
 end  
 
-def 
-
+def self.create_form_filename(filename)
+split_filename = filename.chomp(".mp3").split(" - ")
+  song =Song.new
+  song.name = split_filename[1]
+  song.artist_name =split_filename[0]
+  song.save
+  song
 
 end 
 
